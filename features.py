c1732f6f67f485de444e0ec9a3ae3b06dd077b36
Aquila42 <aquila.khanam@gmail.com>
2017-10-29 15:47:53 -0400

    unigrams

--- features.py
+++ features.py
@@ -1,2 +1,14 @@
+from nltk import word_tokenize
+
+#get ngrams per author - maybe most common words?
+def get_unigrams(df):
+    texts = df['text'].apply(lambda row: row.strip().decode("ascii", "ignore").encode("ascii").lower())
+    unigrams = set()
+    for text in texts.values:
+        for word in word_tokenize(text):
+            if word not in unigrams:
+                unigrams.add(word)
+    return unigrams
+
 # n-grams
 # punctuation
